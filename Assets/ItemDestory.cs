8e4d413aca24418f15047ba1d280db85c12e4a51
関修吾 <15b0228@tku.ac.jp>
2018-05-21 11:21:51 +0900

    StepAsideUnityChanプロジェクト　2回目のコミット
    課題のコード制作

---
+++ Assets/ItemDestory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestory : MonoBehaviour {
+
+	public GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.z < unitychan.transform.position.z) {
+			Destroy (gameObject);
+		}
+	}
+}
